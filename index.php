3704135fd53bef96e12e15545916f9ffe4a9c5da
ph4zan <azazsuper123@gmail.com>
2025-02-10 13:32:03 +0200

    array

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-$string = 'eric.mann@cookbook.php';
-$start = strpos($string, '@');
-$domain = substr($string, $start + 1);
+$strings = array('AKLWC139', 'LMNSDO', 'akwSKWsm');
+foreach ($strings as $testcase) {
+    if (ctype_upper($testcase)) {
+        echo "Строка $testcase состоит только из букв в верхнем регистре.\n";
+    } else {
+        echo "Строка $testcase не состоит только из букв в верхнем регистре.\n";
+    }
+}
